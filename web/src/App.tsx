d3c8d1631595ed029e4cbb3edc646adf1091fbb7
unknown <mickl0102@outlook.com>
2024-01-20 07:50:51 -0600

    frontend initial commit

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { ConfigProvider } from "antd";
+import { Button } from "antd";
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;

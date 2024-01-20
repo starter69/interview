54615571af569880654072c99bbf323b23f7b137
unknown <mickl0102@outlook.com>
2024-01-20 10:14:00 -0600

    Added env

--- web/src/api/index.ts
+++ web/src/api/index.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const host = process.env.REACT_APP_API_HOST || "localhost";
+const port = process.env.REACT_APP_API_PORT || 3001;
+
 const apiService = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: `http://${host}:${port}`,
 });
 
 export const getHelloWorldMsg = () => apiService.get("/hello-world");

e50241f1eb13c1b861db9d750c175c7be11ce64d
Cedric <wangcedric272@gmail.com>
2024-01-21 21:18:16 -0500

    Set Prettier config

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    "linebreak-style": ["error", "unix"],
+    "linebreak-style": ["error", "lf"],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',

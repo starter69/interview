d4eb526e71038b8c443ce43f2b6a9b470727c33b
omitent <mickl0102@outlook.com>
2024-01-23 06:26:47 -0600

    Updated userLogin - Redirect when login succeed

--- web/src/pages/LogIn.tsx
+++ web/src/pages/LogIn.tsx
@@ -2,13 +2,16 @@ import LogInForm from 'components/LogInForm'
 import { userLogin } from 'api'
 import { UserLoginRequest } from 'api/types'
 import { message } from 'antd'
+import { useNavigate } from 'react-router-dom'
 
 const LogIn = () => {
+	const navigate = useNavigate()
 	const handleSubmit = (values: UserLoginRequest) => {
 		userLogin({ name: values.name, password: values.password })
 			.then((response) => {
-				localStorage.setItem('authToken', response.data)
+				localStorage.setItem('authToken', response.data.access_token)
 				message.success('Logged In Successfully')
+				navigate('/dashboard')
 			})
 			.catch((error) => {
 				console.error(error)
